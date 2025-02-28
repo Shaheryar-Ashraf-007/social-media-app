95445dc8c19a0a438eccef4df4f0f5d7984516d7
Shaheryar-Ashraf-007 <shaheryarashraf824@gmail.com>
2025-02-28 22:15:23 +0500

    complete frontend

--- social-app/src/app/profile/[id]/page.tsx
+++ social-app/src/app/profile/[id]/page.tsx
@@ -1,20 +1,48 @@
 import React from 'react'
 import LeftMenu from '../../../components/LeftMenu'
 import RightMenu from '../../../components/RightMenu'
-import AddPost from '../../../components/AddPost'
-import Stories from '../../../components/Stories'
+// import AddPost from '../../../components/AddPost'
+// import Stories from '../../../components/Stories'
 import Feed from '../../../components/Feed'
-
+import Image from 'next/image'
+import image5 from "../../../../public/image5.jpg"
+import image6 from "../../../../public/image6.jpg"
 const Profilepage = () => {
   return (
     <div className='flex gap-6 PT-6'>
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu/>
+        <LeftMenu type='profile'/>
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
-          <Stories/>
-            <AddPost/>
+          <div className=" mt-8 flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image src={image5} alt='' fill className="rounded-md"/>
+              <Image src={image6} alt='' width={128} height={128} objectFit="cover" className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white"/>
+            </div>
+            <h1 className='mt-20 mb-4 text-2xl font-medium'>Shane Watson</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>123</span>
+                <span className='text-sm'>Post</span>
+
+              </div>
+
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>1.3k</span>
+                <span className='text-sm'>Followers</span>
+
+              </div>
+
+              <div className="flex flex-col items-center">
+                <span className='font-medium'>10</span>
+                <span className='text-sm'>Following</span>
+
+              </div>
+
+
+            </div>
+          </div>
             <Feed/>
         </div>
       </div>
--- social-app/src/components/ProfileCard.tsx
+++ social-app/src/components/ProfileCard.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import image7 from "../../public/image7.jpg";
+import image8 from "../../public/image8.jpg";
 
 const ProfileCard = () => {
   return (
     <div>
-        <div className="">Profile card</div>
-    </div>
-  )
-}
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6 mt-8">
+        <div className="h-20 relative">
+          <Image src={image7} alt="" fill className="rounded-md" />
+          <Image
+            src={image8}
+            alt=""
+            width={48}
+            height={48}
+            className="rounded-full w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+          />
+        </div>
+        <div className="h-20 flex flex-col gap-2 items-center">
+          <span className="font-semibold">Edward Gabriel May</span>
+          <div className="flex items-center gap-4">
+            <div className="flex">
+              <Image
+                src={image8}
+                alt=""
+                width={12}
+                height={12}
+                className="rounded-full object-cover w-3 h-3"
+              />
 
-export default ProfileCard
+              <Image
+                src={image8}
+                alt=""
+                width={12}
+                height={12}
+                className="rounded-full object-cover w-3 h-3"
+              />
+
+              <Image
+                src={image8}
+                alt=""
+                width={12}
+                height={12}
+                className="rounded-full object-cover w-3 h-3"
+              />
+            </div>
+            <span className="text-xs text-gray-500">500 Followers</span>
+          </div>
+          <button className="bg-blue-500 rounded-lg p-2 text-white">My Profile</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
